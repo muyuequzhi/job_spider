f43c4703ee8f65f7c49579223151c3056c9e1d87
feng <linqu66@gmail>
2019-04-16 21:44:37 +0800

    需要进行大的改动，提前备份下

--- test1.py
+++ test1.py
@@ -1,15 +1,17 @@
-import jieba
-from pprint import pprint
+from bs4 import BeautifulSoup
 import csv
-post = open("post_require.txt", "r", encoding="utf-8").read()
-file_path = "user_dict.txt"
-jieba.load_userdict(file_path)
-seg_list = jieba.cut(post, HMM=False)
-counter = dict()
-for seg in seg_list:
-    counter[seg] = counter.get(seg, 1) + 1
-counter_sort = sorted(counter.items(), key=lambda value: value[1], reverse= True)
-pprint(counter_sort)
-with open("post_counter.csv", "w+", encoding="utf-8") as f:
-    f_csv = csv.writer(f)
-    f_csv.writerows(counter_sort)
+import requests
+"爬取薪资、经验、职位、位置"
+html = open("test.html").read()
+soup = BeautifulSoup(html,"lxml")
+salary = soup.find("div",class_="cn").find("strong").text
+post = soup.find("div",class_="cn").find("h1").text
+txt = soup.find(class_="msg ltype").text.strip()
+c_txt = txt.replace("|",",")
+t_txt = salary + "," + post +","  + c_txt
+d_txt = "".join(t_txt.split())
+e_txt = d_txt + "\n"
+with open("test.csv","w",encoding="utf-8") as f:
+    f.write(e_txt)
+    f.write(e_txt)
+    f.close()
